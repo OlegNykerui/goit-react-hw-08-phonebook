9eca75daeee5ec17f89f04479826a4702c024a7d
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-12-14 01:48:47 +0200

    fix store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,61 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import logger from 'redux-logger';
-import { filterReducer } from './reducers';
-import { contactsApi } from 'redux/api';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsReducer } from './contacts/contactsSlice';
+import { authReducer } from './auth/authSlice';
 
-const middleware = getDefaultMiddleware =>
-  getDefaultMiddleware({}).concat(contactsApi.middleware, logger);
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
-    [contactsApi.reducerPath]: contactsApi.reducer,
-    filter: filterReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
-  // middleware: getDefaultMiddleware => [
-  //   ...getDefaultMiddleware(),
-  //   contactsApi.middleware,
-  //   logger,
-  // ],
 });
 
-setupListeners(store.dispatch);
+export const persistor = persistStore(store);
+
+// export const store = configureStore({
+//   reducer: {
+//     [contactsApi.reducerPath]: contactsApi.reducer,
+//     filter: filterReducer,
+//   },
+//   middleware,
+//   devTools: process.env.NODE_ENV === 'development',
+// middleware: getDefaultMiddleware => [
+//   ...getDefaultMiddleware(),
+//   contactsApi.middleware,
+//   logger,
+// ],
+// });
+
+// const middleware = getDefaultMiddleware =>
+//   getDefaultMiddleware({}).concat(contactsApi.middleware, logger);
+
+// setupListeners(store.dispatch);
 
 // import { contactsReducer } from './slices/contactSlice';
 // import {
